c1e93440b1c417b50671636613deec58894e66a8
Aaron <114060853+hecabre@users.noreply.github.com>
2023-05-25 12:51:33 +0800

    Delete NavbarItem.jsx

--- NavbarItem.jsx
+++
@@ -1,9 +0,0 @@
-import { Link } from "react-router-dom"
-
-function NavbarItem({path}) {
-  return (
-    <Link to={path} className="text-white">{path.slice(1)}</Link>
-  )
-}
-
-export default NavbarItem
